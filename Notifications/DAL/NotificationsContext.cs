8087d9dea867bfcd79e72747da6ef0d4be3e29d4
Sergio Magaña <sergiotem@gmail.com>
2015-10-10 23:41:19 +0200

    Add NotificationsContext

---
+++ Notifications/DAL/NotificationsContext.cs
@@ -0,0 +1,18 @@
+﻿using Notifications.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Notifications.DAL
+{
+    public class NotificationsContext : DbContext
+    {
+        public DbSet<Notification> Notifications { get; set; }
+
+        public NotificationsContext() : base ("Notifications")
+        {
+        }
+    }
+}
