4a0ce71ddd0ed606646f82a1de92e0bd264dbd45
Sergio Magaña <sergiotem@gmail.com>
2015-10-10 23:42:34 +0200

    Add notification model structure

--- Notifications/Models/Notification.cs
+++ Notifications/Models/Notification.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,17 @@ namespace Notifications.Models
 {
     public class Notification
     {
+        [Key]
+        public Guid? Id { get; private set; }
+        public DateTime CreationDate { get; private set; }
+        public string Type { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Notification()
+        {
+            Id = Guid.NewGuid();
+            CreationDate = DateTime.Now;
+        }
     }
 }
