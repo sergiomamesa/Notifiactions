405c22fd0e2dab84bb6631881fb3893d854a9159
Sergio Magaña <sergiotem@gmail.com>
2015-10-10 23:41:41 +0200

    Add WebApiConfig

---
+++ Notifications/App_Start/WebApiConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Notifications
+{
+    public class WebApiConfig
+    {
+        public static void Register(HttpConfiguration configuration)
+        {
+            configuration.Routes.MapHttpRoute("API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+        }
+    }
+}
